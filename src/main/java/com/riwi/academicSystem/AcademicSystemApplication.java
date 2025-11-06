6b9d41480eeb620c51807a4295330f005631c71c
osvaldocs <pablo_campos92@hotmail.com.ar>
2025-11-06 13:09:42 -0500

    first commit

---
+++ src/main/java/com/riwi/academicSystem/AcademicSystemApplication.java
@@ -0,0 +1,13 @@
+package com.riwi.academicSystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AcademicSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AcademicSystemApplication.class, args);
+	}
+
+}
