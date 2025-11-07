527930a3c873d4e84f7e44784f3bf7a1fb349879
osvaldocs <pablo_campos92@hotmail.com.ar>
2025-11-07 13:44:50 -0500

    repository

---
+++ src/main/java/com/riwi/academicSystem/infrastructure/CourseRepository.java
@@ -0,0 +1,6 @@
+package com.riwi.academicSystem.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseRepository extends JpaRepository {
+}
---
+++ src/main/java/com/riwi/academicSystem/infrastructure/ProfessorRepository.java
@@ -0,0 +1,6 @@
+package com.riwi.academicSystem.infrastructure;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfessorRepository extends JpaRepository {
+}
